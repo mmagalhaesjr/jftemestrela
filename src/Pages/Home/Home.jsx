8277da76bb0de6bb6fc526d1ce8433f16031b317
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2025-03-10 22:39:38 -0300

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,22 @@
-
 import Header from "../../components/Header/Header";
 import { StyledHome } from "./styled";
-import jf from '../../assets/jf.png'
-import estrela from '../../assets/logo.png'
-
-
+import jf from '../../assets/jf.png';
+import estrela from '../../assets/logo.png';
 
 export default function Home() {
+    const elementos = [];
+    
+    for (let i = 0; i < 7; i++) {
+        elementos.push(
+            <div className="caixaTexto" key={i}>
+                <p>SUA OPORTUNIDADE DE SUCESSO!</p>
+                <img id="estrela" src={estrela} alt="img" />
+            </div>
+        );
+    }
+
     return (
         <StyledHome>
-
-
             <section id="container">
 
                 <Header />
@@ -19,79 +25,13 @@ export default function Home() {
 
                 <div id="texto">
                     <div className="wrapper">
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-                        <div className="caixaTexto">
-                            <p>
-                                SUA OPORTUNIDADE DE SUCESSO!
-                            </p>
-
-                            <img id="estrela" src={estrela} alt="img" />
-                        </div>
-
-
-
-
+                        {elementos}
                     </div>
                 </div>
-
-
-                <h2 id="botao">
-                    INSCRICÕES EM BREVE
-                </h2>
-
+                
+                <h2 id="botao">INSCRIÇÕES EM BREVE</h2>
 
             </section>
-
-
-
         </StyledHome>
-    )
+    );
 }
--- src/Pages/Home/styled.js
+++ src/Pages/Home/styled.js
@@ -1,119 +1,102 @@
 import styled from "styled-components";
 import { roxo, verde } from '../../constants/Cores';
 
-
 export const StyledHome = styled.main`
-width: 100%;
-height: 100vh;
-background-color: ${verde};
-
-display: flex;
-align-items: center;
-justify-content: center;
-
-
-#container{
-    width: 90%;
-    height: 90%;
-    background-color: ${roxo};
-    border-radius: 50px;
-    position: relative;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-}
-
-#imgJf{
-    width: 50%;
-    height: 50%;
-}
-
-
-@keyframes animar {
-    0% {
-        transform: translateX(0);
-    }
-    100% {
-        transform: translateX(-100%);
-    }
-}
-
-#texto{
-    width: 100vw;
-    display: flex;
-    align-items: flex-start;
-    overflow: hidden;
-   
-    position: relative;
-    margin-top: 20px;
-}
-.wrapper {
-    display: flex;
-    width: calc(100% * 2); /* Duas vezes o tamanho do container para looping */
-    animation: animar 20s linear infinite;
-}
-.caixaTexto{
-    width: 33,33%; 
-    flex-shrink: 0;
-
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-}
-
-#botao{
-    width: fit-content;
-    height: fit-content;
-    background-color: ${verde};
-    border: none;
-    outline: none;
-    color: #000000;
-    padding: 10px;
-    cursor: pointer;
-    font-size: 2rem;
-
-    &:hover{
-        background-color: #46df09;
-        color: #ffffff;
-    }
-}
-
-#texto{
     width: 100%;
-    height: fit-content;
-    background-color: transparent;
+    height: 100vh;
+    background-color: ${verde};
 
     display: flex;
     align-items: center;
-    justify-content: space-evenly;
-}
-p{
-    color: #000000;
-    font-size: 1.5rem;
-    font-family: "Inter", sans-serif;
-    font-style: italic;
-}
-#estrela{
-    width: 50px;
-}
+    justify-content: center;
 
+    #container {
+        width: 90%;
+        height: 90%;
+        background-color: ${roxo};
+        border-radius: 50px;
+        position: relative;
 
-@media (max-width:800px) {
-
-    #imgJf{
-    width: 80%;
-    height: 30%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
     }
 
-    p{
-        font-size: 1rem;
-       
+    #imgJf {
+        width: 50%;
+        height: 50%;
     }
-    #botao{
-        font-size:1.2rem;
-    }
-}
 
-`
+    #texto {
+        width: 100vw;
+        overflow: hidden;
+        margin-top: 20px;
+        display: flex;
+        align-items: center;
+    }
+
+    .wrapper {
+        display: flex;
+        width: 200%; /* O dobro para garantir o looping */
+        animation: animar 10s linear infinite;
+    }
+
+    .marquee {
+        display: flex;
+        width: 100%;
+    }
+
+    .caixaTexto {
+        width: 14%; /* 7 elementos por loop */
+        flex-shrink: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    @keyframes animar {
+        from {
+            transform: translateX(0);
+        }
+        to {
+            transform: translateX(-50%);
+        }
+    }
+
+    #botao {
+        background-color: ${verde};
+        padding: 10px;
+        cursor: pointer;
+        font-size: 2rem;
+        &:hover {
+            background-color: #46df09;
+            color: #ffffff;
+        }
+    }
+
+    p {
+        font-size: 1.5rem;
+        font-family: "Inter", sans-serif;
+        font-style: italic;
+    }
+
+    #estrela {
+        width: 50px;
+    }
+
+    @media (max-width: 800px) {
+        #imgJf {
+            width: 80%;
+            height: 30%;
+        }
+
+        p {
+            font-size: 1rem;
+        }
+
+        #botao {
+            font-size: 1.2rem;
+        }
+    }
+`;
