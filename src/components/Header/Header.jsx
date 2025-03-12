6b53778b80a056bb9e4aa4d6eb6b9f34931b79b4
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2025-03-12 10:02:34 -0300

    tiktok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
 
     const whatsapp = "https://api.whatsapp.com/send?phone=32999828888"
     const inst ="https://www.instagram.com/jftemestrela?igsh=MW91c3d6NHZ0bGYxcA=="
-    const tiktok = ''
+    const tiktok = 'https://www.tiktok.com/@musicboxstudiobr'
 
     return(
         <StyledHeader>
